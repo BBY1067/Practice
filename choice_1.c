e4685605806e07a5afd9654f8a2ff0b96d991256
unknown <200000338@qq.com>
2022-01-28 21:36:17 +0800

    BBY1067

---
+++ choice_1.c
@@ -0,0 +1,52 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<string.h>
+#include<stdio.h>
+//输入一个数，判断是奇数还是偶数
+// int main()
+//{
+//	int a = 1;
+//	int b = 1;
+//	scanf("%d", a);
+//	if (a % 2 == 1)
+//		printf("%d是奇数\n", a);
+//	else
+//		printf("%d是偶数\n", a);
+//	return 0;
+
+
+
+// 求出1到100中的奇数
+//int main()
+//{
+//	int i = 1;
+//	while (i < 101)
+//	{
+//		if (i % 2 == 1)
+//			printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+
+//switch语句：查看某个年龄段是否被允许
+// int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (0 <= a && a <= 100)
+//		switch (a)
+//		{
+//		case 14:
+//		case 15:
+//		case 16:
+//		case 17:
+//		case 18:printf("old enough\n");
+//			break;
+//			default: printf("you are not allowed\n");
+//		}
+//	else
+//		printf("make sure you type the right number\n");
+//	return 0;
+//}
